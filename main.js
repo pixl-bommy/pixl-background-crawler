cba78f78d3a82a00777345a54b417dbde3db70ce
Thomas Borck <mail@bommy.net>
2019-10-26 05:59:23 +0200

    Initial setup of electron window.

---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow = null;
+
+/**
+ * Create a new main window and open index.html.
+ */
+const createWindow = () => {
+    const windowPreferences = {
+        width: 960,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    };
+
+    mainWindow = new BrowserWindow(windowPreferences);
+
+    // destroy window reference, if window was closed
+    mainWindow.on("closed", () => mainWindow = null);
+
+    mainWindow.loadFile("./public/index.html");
+    mainWindow.webContents.openDevTools();
+}
+
+/**
+ * Exit app, if all windows were closed.
+ * Info: If we're running on macOS, the app will be paused and not destroyed.
+ */
+app.on("window-all-closed", () => {
+    if (process.platform === "darwin") return;
+    app.quit();
+});
+
+/**
+ * Create the main window on macOS, after the app was 'paused' and reactivated.
+ */
+app.on("activate", () => {
+    if (mainWindow === null)
+        createWindow();
+});
+
+/**
+ * Initial create the main window on app start up.
+ */
+app.on("ready", createWindow);
